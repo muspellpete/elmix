6d1c9f4dee25cb3f7a79e66232f7df4567d7bfe7
muspellpete <muspell_pete@hotmail.com>
2019-07-07 09:41:42 +0200

    Began implementing a basic query, not functional
    Implementation based on example https://github.com/dillonkearns/elm-graphql/blob/master/examples/src/Example01BasicQuery.elm

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,6 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import CustomScalarCodecs exposing {Id}
+import Graphql.Document as Document
+import Graphql.Http
+import Graphql.Operation exposing {RootQuery}
+import Graphql.SelectionSet as SelectionSet exposing {SelectionSet}
 
 main =
  text "Hello from Elmo 2"
+
+query: SelectionSet Response RootQuery
+query = Query
