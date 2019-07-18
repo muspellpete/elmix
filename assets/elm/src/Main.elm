213577001751b483229cfbccd10464db08e04f6a
muspellpete <muspell_pete@hotmail.com>
2019-07-18 23:02:29 +0200

    Added PrintAny, and made code compile further
    PrintAny was copied from DillonKearns, who had made a working version for newer Elm versions based on some older library.
    
    Also made several changes to the Main.elm script to make it run, such as fixing init vs. initTwo issues etc. And attempting to fix issues with samplesInfoSelection (now named weatherTypeInfoSelection, because the point is that it should extract the fields inside a WeatherType object.

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,37 +1,142 @@
 module Main exposing (..)
 
-import Html exposing (..)
+--import Html exposing (..)
 import Graphql.Document as Document
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
-import Api.Interface
+import Api.Object
 import Api.Object.WeatherType as WeatherType
 import Api.Query as Query
+import Api.ScalarCodecs exposing (Id)
+
+import Browser
+--import DateFormat exposing (text)
+import Html exposing (Html, a, div, h1, input, label, p, pre, text)
+import Html.Attributes exposing (href, type_)
+import Html.Events exposing (onClick)
+import RemoteData exposing (RemoteData)
+import PrintAny
+import Regex
+
+type alias Program flags subModel subMsg =
+    Platform.Program flags (ModelTwo subModel) (MsgTwo subMsg)
+
+type MsgTwo subMsg
+    = ToggleAliases
+    | SubMsg subMsg
+
+type alias ModelTwo subModel =
+    { subModel : subModel
+    , hideAliases : Bool
+    }
+
+document : 
+    { initTwo : flags -> ( subModel, Cmd subMsg )
+    , updateTwo : subMsg -> subModel -> ( subModel, Cmd subMsg )
+    , queryString : String
+    }
+    -> Program flags subModel subMsg
+
+document { initTwo, updateTwo, queryString } =
+    Browser.document
+    { init = mapInit initTwo
+    , update = mapUpdate updateTwo
+    , subscriptions = \_ -> Sub.none
+    , view = view queryString
+    }
+
+mapInit : (flags -> ( subModel, Cmd subMsg )) -> flags -> ( ModelTwo subModel, Cmd (MsgTwo subMsg))
+mapInit subInit flags =
+    subInit flags
+        |> Tuple.mapFirst (\subModel -> { subModel = subModel, hideAliases = True })
+        |> Tuple.mapSecond (Cmd.map SubMsg)
+
+mapUpdate : (subMsg -> subModel -> ( subModel, Cmd subMsg )) -> MsgTwo subMsg -> ModelTwo subModel -> ( ModelTwo subModel, Cmd (MsgTwo subMsg))
+mapUpdate subUpdate msg model =
+    case msg of
+        ToggleAliases ->
+            ( { model | hideAliases = not model.hideAliases}, Cmd.none )
+
+        SubMsg subMsg ->
+            let 
+                (a, b) =
+                    subUpdate subMsg model.subModel
+
+            in
+            ({model | subModel = a}, b |> Cmd.map SubMsg)
+
+view : String -> ModelTwo a -> Browser.Document (MsgTwo subMsg)
+view queryTwo model =
+    { title = "Query Explorer"
+    , body =
+        [ div []
+            [ div []
+                [ h1 [] [text "Generated Query"]
+                , p [] [toggleAliasesCheckbox ]
+                , pre []
+                    [ (if model.hideAliases then 
+                         queryTwo |> stripAliases
+                      else
+                        queryTwo
+                        )
+                        |> text
+
+                    ]
+                ]
+                , div []
+                     [ h1 [] [text "Response"]
+                     , model.subModel |> PrintAny.view
+                     ]
+            ]
+        ]
+    }
+
+toggleAliasesCheckbox : Html (MsgTwo subMsg)
+toggleAliasesCheckbox =
+    label []
+    [ input [type_ "checkbox", onClick ToggleAliases] []
+    , text " Show Aliases"
+    , a [ href "https://github.com/dillonkearns/elm-graphql/blob/master/FAQ.md#how-do-field-aliases-work-in-dillonkearnselm-graphql" ]
+        [ text "(?)"
+        ]
+    ]
+
+stripAliases : String -> String
+stripAliases queryTwo =
+    queryTwo
+        |> Regex.replace
+            (Regex.fromStringWith { multiline = True, caseInsensitive = True }, "^(\\s*)\\w+: "
+                |> Maybe.withDefault Regex.never
+            )
+            (\match -> match.submatches |> List.head |> Maybe.withDefault Nothing |> Maybe.withDefault "")
+
 
 type alias Response =
-    WeatherType
+    WeatherTypeTwo
 
 query : SelectionSet Response RootQuery
 query =
-    Query.samples identity samplesInfoSelection
+    Query.samples identity weatherTypeInfoSelection
 
-type alias WeatherType =
-    { cloudy: bool
-    , id : id
+type alias WeatherTypeTwo =
+    { cloudy : Bool
+    , id : Id
     , moisture : Int
     }
 
-samplesInfoSelection : SelectionSet WeatherType Api.Interface.placeholder
-samplesInfoSelection =
-    SelectionSet.map3 WeatherType
-    WeatherType.name
-    WeatherType.id
+weatherTypeInfoSelection : SelectionSet WeatherTypeTwo Api.Object.WeatherType
+weatherTypeInfoSelection =
+    SelectionSet.map3 WeatherTypeTwo
+        WeatherType.cloudy
+        WeatherType.id
+        WeatherType.moisture
 
-makeRequest : Cmd type Msg
-    = query
-    |> Graphql.Http.queryRequest "https://localhost:4000/api/graphql"
-    |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
+makeRequest : Cmd Msg
+makeRequest =
+    query
+        |> Graphql.Http.queryRequest "https://localhost:4000/api/graphql"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
 
 type Msg
     = GotResponse Model
@@ -39,7 +144,7 @@ type Msg
 type alias Model =
     RemoteData (Graphql.Http.Error Response) Response
 
-type alias Flas =
+type alias Flags =
     ()
 
 init : Flags -> ( Model, Cmd Msg )
@@ -52,10 +157,10 @@ update msg model =
         GotResponse response ->
             ( response, Cmd.none )
 
-main : Helpers.Main.Program Flags Model Msg
+main : Program Flags Model Msg
 main =
-    Helpers.Main.document
-        { init = init
-        , update = update
+    document
+        { initTwo = init
+        , updateTwo = update
         , queryString = Document.serializeQuery query
         }
